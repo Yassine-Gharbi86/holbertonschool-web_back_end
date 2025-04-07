4ba9952e17a31350a0fffc18a3e664161ed791e7
root <root@DESKTOP-NDVQC7F.localdomain>
2025-04-07 13:25:38 +0200

    taks

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,13 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-      .filter((student) => student.location === city)
-      .map((student) => {
-        const gradeObj = newGrades.find((g) => g.studentId === student.id);
-        const grade = gradeObj ? gradeObj.grade : 'N/A';
-  
-        return {
-          ...student,
-          grade,
-        };
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((g) => g.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+
+      return {
+        ...student,
+        grade,
+      };
     });
 }
