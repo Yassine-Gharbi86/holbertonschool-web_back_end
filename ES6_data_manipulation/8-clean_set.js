d67f80eee241f9ed77178a6d56b8a1806ebcb9e0
root <root@DESKTOP-NDVQC7F.localdomain>
2025-04-07 13:32:49 +0200

    task

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') {
     return '';
   }
-  
+
   const filtered = Array.from(set)
     .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length));
